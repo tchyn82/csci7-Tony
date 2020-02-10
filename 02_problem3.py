6f8bb6b71a2bb7a259b2abe9bf3b457f3acb6152
Tony <tonychyn@gmail.com>
2020-02-09 21:33:46 -0500

    problem3

---
+++ 02_problem3.py
@@ -0,0 +1,10 @@
+# comparing two values using variables a and b
+a = 5
+b = 34
+
+if a > b: #boolean evaluation of whether a is greater than b
+	print('a is greater than b')
+elif a < b: 
+	print('a is smaller than b')
+else:
+	print('a and b are equal')
