ed624dc761e30911b82c30b82ff8fc43e6c8a8d2
Tony <tonychyn@gmail.com>
2020-02-19 20:50:13 -0500

    P3A3

---
+++ 03_problem3.py
@@ -0,0 +1,12 @@
+my_list = ['Tony', 'Horng', 'Chyn']
+new_list = my_list
+new_list = my_list[0:3]
+new_list = my_list[0:]
+new_list = my_list[:3]
+new_list = my_list[:]
+new_list = my_list[0]
+new_list = my_list[0:2]
+new_list = my_list[1:]
+new_list = my_list[-1]
+new_list = my_list[:-1]
+print('test')
